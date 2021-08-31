5de3c95a9b64ec45c75d5d5d793a2889aa26171e
hyukzz <ri_chard@naver.com>
2021-08-31 22:55:00 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Apple from './Apple';
 
+function Food({favourite}) {
+  return <h2>I love {favourite}</h2>
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+    <h1>안녕?</h1>
+    <Apple />
+    <Food favourite = "초밥" />
+    <Food favourite = "고기" />
+    <Food favourite = "젤리" />
     </div>
-  );
+    ); 
 }
 
 export default App;
---
+++ src/Apple.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Apple() {
+    return <h3>i love Apple</h3>
+}
+
+export default Apple;
