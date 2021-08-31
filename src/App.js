1ce1e3f6e8290e1681d831902d61500d8c808816
hyukzz <ri_chard@naver.com>
2021-09-01 00:48:40 +0900

    #2 JSX & PROPS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import Apple from './Apple';
+import PropTypes from 'prop-types';
 
-function Food({favourite}) {
-  return <h2>I love {favourite}</h2>
+function Food({name, picture, rating}) {
+  return (
+    <div>
+      <h2>I love {name}</h2>
+      <img src = {picture} alt = {name}/>
+      <h3>{rating}/5</h3>
+    </div>
+  )
 }
+Food.protoTypes ={
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number
+}
+
+const foodILike = [
+  {
+    id : 1,
+    name : "초밥",
+    image:
+    "https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg",
+    rating : 4.7
+  },
+  {
+    id: 2,
+    name : "고기",
+    image :
+    "https://cdn.pixabay.com/photo/2016/03/05/23/02/barbecue-1239434_1280.jpg",
+    rating : 4.5
+  },
+  {
+    id: 3,
+    name : "젤리",
+    image :
+    "https://cdn.pixabay.com/photo/2018/06/19/23/25/sugar-3485430_1280.jpg",
+    rating : 5
+  }
+]
+
 function App() {
   return(
     <div>
     <h1>안녕?</h1>
     <Apple />
-    <Food favourite = "초밥" />
-    <Food favourite = "고기" />
-    <Food favourite = "젤리" />
+    {foodILike.map(dish => (
+    <Food 
+    key={dish.id} 
+    name = {dish.name} 
+    picture ={dish.image}
+    rating ={dish.rating} />)
+      )}
     </div>
     ); 
 }
