a4b374777a79873e7fc751775fd0831af9d83773
hyukzz <ri_chard@naver.com>
2021-09-02 02:05:40 +0900

    #3State_1

--- src/App.js
+++ src/App.js
@@ -1,60 +1,34 @@
 import React from 'react';
-import Apple from './Apple';
-import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}) {
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <img src = {picture} alt = {name}/>
-      <h3>{rating}/5</h3>
-    </div>
-  )
-}
-Food.protoTypes ={
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number
-}
+class App extends React.Component{
+  state = {
+    count: 0
+  };
 
-const foodILike = [
-  {
-    id : 1,
-    name : "초밥",
-    image:
-    "https://cdn.pixabay.com/photo/2016/04/26/03/55/salmon-1353598_1280.jpg",
-    rating : 4.7
-  },
-  {
-    id: 2,
-    name : "고기",
-    image :
-    "https://cdn.pixabay.com/photo/2016/03/05/23/02/barbecue-1239434_1280.jpg",
-    rating : 4.5
-  },
-  {
-    id: 3,
-    name : "젤리",
-    image :
-    "https://cdn.pixabay.com/photo/2018/06/19/23/25/sugar-3485430_1280.jpg",
-    rating : 5
+  add = () => {
+    this.setState(current =>({count: current.count + 1}))
+  };
+  minus = () => {
+    this.setState(current =>({count: current.count - 1}))
+  };
+
+  componentDidMount(){
+    console.log('마운트')
   }
-]
-
-function App() {
-  return(
+  componentDidUpdate(){
+    console.log('업데이트')
+  }
+  
+  render(){
+    console.log("렌더링")
+    return(    
     <div>
-    <h1>안녕?</h1>
-    <Apple />
-    {foodILike.map(dish => (
-    <Food 
-    key={dish.id} 
-    name = {dish.name} 
-    picture ={dish.image}
-    rating ={dish.rating} />)
-      )}
-    </div>
-    ); 
+      <h1>숫자 매크로   :   {this.state.count}</h1> 
+      <button onClick={this.add}>더하기</button>
+      <button onClick={this.minus}>빼기</button>
+      </div>
+      )
+  }
 }
 
 export default App;
